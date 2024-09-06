0f44d21daa070198dff7a9c55db6fdcbb941ae23
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-09-06 09:29:30 +0200

    Add project files.

---
+++ Utozas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Xps.Serialization;
+
+namespace EUtazas2020
+{
+    internal class Utozas
+    {
+        public int Sorszam { get; set; }
+        public string Datum { get; set; }
+
+        public int Azonosito { get; set; }
+
+        public string JegyTipus { get; set; }
+
+        public int Ervenyesseg { get; set; }
+
+        public Utozas(string sorok)
+        {
+            var sor = sorok.Split(' ');
+            Sorszam = Convert.ToInt32(sor[0]);
+            Datum = sor[1];
+            Azonosito = Convert.ToInt32(sor[2]);
+            JegyTipus = sor[3];
+            Ervenyesseg = Convert.ToInt32(sor[4]);
+        }
+
+    }
+}
